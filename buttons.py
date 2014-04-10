86c7f0ccc5b1089ae7a04875cb4f752577207e46
Guest <guest-jewG25@meet.mit.edu>
2014-04-10 17:46:44 +0300

    clicking logic

--- buttons.py
+++ buttons.py
@@ -1,13 +1,16 @@
 import pygame
 
 class button(object):
-	def __init__(self, xlocation, ylocation, x2size, y2size):
+	def __init__(self, xlocation, ylocation, x2size, y2size,r,g,b):
 		self.xlocation=xlocation
 		self.ylocation=ylocation
 		self.x2size=x2size
 		self.y2size=y2size
 		self.rec = pygame.Rect(xlocation, ylocation, x2size, y2size)
    		self.sq = pygame.Surface([x2size, y2size])
+   		self.sq.fill((r,g,b))
 	def draw(self,main_screen):
 		main_screen.blit(self.sq, self.rec)
+
     
+
--- ex.py
+++ ex.py
@@ -9,23 +9,29 @@ if __name__=="__main__":
     # button_sq = pygame.Surface([100, 100])
     # main_screen.blit(button_sq, button_rec)
     
-    B1 = buttons.button(150, 100, 200, 200)
+    B1 = buttons.button(150, 100, 200, 200,0, 0, 0)
     B1.draw(main_screen)
 
-    B2 = buttons.button(650,100,200,200)
+    B2 = buttons.button(650,100,200,200, 0, 0, 0)
     B2.draw(main_screen)
 
-    B3 = buttons.button(150, 400,200,200)
+    B3 = buttons.button(150, 400,200,200, 0, 0, 0)
     B3.draw(main_screen)
 
-    B4 = buttons.button(650,400,200,200)
+    B4 = buttons.button(650,400,200,200, 0, 0, 0)
     B4.draw(main_screen)
 
-    B5 = buttons.button(150,700,200,200)
+    B5 = buttons.button(150,700,200,200, 0, 0, 0)
     B5.draw(main_screen)
 
-    B6 =buttons.button(650,700,200,200)
+    B6 =buttons.button(650,700,200,200, 0, 0, 0)
     B6.draw(main_screen)
+
+    B7=buttons.button(0,0,1000,1000, 255,255, 255)
+
+
+
+    
 
 
     while True: 
@@ -35,22 +41,30 @@ if __name__=="__main__":
             x, y = ev.pos
             
             if B1.rec.collidepoint(x, y):
-                print "I clicked B1"
-
+               B7.draw(main_screen)
+               B1.draw(main_screen)
+                
+                
             if B2.rec.collidepoint(x,y):
-                print "I clicked B2!"
+               B7.draw(main_screen)
+               B2.draw(main_screen)
 
             if B3.rec.collidepoint(x,y):
-                print "I clicked B3!"
+               B7.draw(main_screen)
+               B3.draw(main_screen)
 
             if B4.rec.collidepoint(x,y):
-                print "I clicked B4!"
+               B7.draw(main_screen)
+               B4.draw(main_screen)
 
             if B5.rec.collidepoint(x,y):
-                print "I clicked B5!"
+               B7.draw(main_screen)
+               B5.draw(main_screen)
 
             if B6.rec.collidepoint(x,y):
-                print "I clicked B6!"
+               B7.draw(main_screen)
+               B6.draw(main_screen)
 
 
         pygame.display.flip()
+ 
